8cfd6683708fcd93a661dc4482e99aed92871341
jtrognon <julietrognon74@gmail.com>
2024-05-08 14:07:40 +0200

    basic forms for story + character

--- vue/admin_fct_vue.js
+++ vue/admin_fct_vue.js
@@ -1,16 +1,99 @@
-function create_story_form_vue(){
-    // Nom des fields
-    let tr_names = ["name"];
-    let tr_display_names = ["Nom de l'histoire"];
 
-    create_form_vue(tr_display_names, tr_names);
+// -----------------------------------------------------------------------
+// -----------------------------------------------------------------------
+// Fonction appelé par la page pour créer les forms avec des champs précis
+// -----------------------------------------------------------------------
+// -----------------------------------------------------------------------
+
+// --------
+// Histoire
+// --------
+
+function create_story_form_vue(){
+    let section = document.querySelector("#story");
+
+    // Nom des fields
+    let tr_names = ["name", ""];
+    let tr_display_names = ["Nom de l'histoire : ", ""];
+    let tr_types = ["text", "submit"]
+    let tr_values = ["", "Ajouter"]
+
+    create_form_vue(section, tr_display_names, tr_names, tr_types, tr_values); 
 }
 
 
-function create_form_vue(tr_display_names, tr_names){
+function update_story_form_vue(tr_values){
+    let section = document.querySelector("#story");
+
+    // Nom des champs
+    let tr_names = ["id", "name", ""];
+    let tr_display_names = ["Identifiant : ", "Nom de l'histoire : ", ""];
+    let tr_types = ["text", "text", "submit"];
+
+    create_form_vue(section, tr_display_names, tr_names, tr_types, tr_values)
+}
+
+function delete_story_form_vue(tr_values){
+    let section = document.querySelector("#story");
+
+    // Nom des champs
+    let tr_names = ["id", ""];
+    let tr_display_names = ["Identifiant : ", ""];
+    let tr_types = ["text", "submit"];
+
+    create_form_vue(section, tr_display_names, tr_names, tr_types, tr_values);
+}
+
+
+// ---------
+// Character
+// ---------
+
+function create_char_form_vue(){
+    let section = document.querySelector("#character");
+
+    // Noms des champs
+    let tr_names = ["image", "name", "color", ""];
+    let tr_display_names = ["URL de l'image : ", "Nom du personnage : ", "Couleur du nom : ", ""];
+    let tr_types = ["text", "text", "text", "submit"];
+    let tr_values = ["", "", "", "Ajouter"];
+
+    create_form_vue(section, tr_display_names, tr_names, tr_types, tr_values);
+}
+
+
+function update_char_form_vue(tr_values){
+    let section = document.querySelector("#character");
+
+    // Noms des champs 
+    let tr_names = ["id", "image", "name", "color", ""];
+    let tr_display_names = ["Identifiant : ", "URL de l'image : ", "Nom du personnage : ", "Couleur du nom : ", ""];
+    let tr_types = ["text", "text", "text", "text", "submit"];
+
+    create_form_vue(section, tr_display_names, tr_names, tr_types, tr_values);
+}
+
+function delete_char_form_vue(tr_values){
+    let section = document.querySelector("#character");
+
+    // Noms des champs
+    let tr_names = ["id", ""];
+    let tr_display_names = ["Identifiant : ", ""];
+    let tr_types = ["text", "submit"];
+
+    create_form_vue(section, tr_display_names, tr_names, tr_types, tr_values);
+}
+
+
+// -------------------------------------------
+// -------------------------------------------
+// Fonctions qui servent à créer un formulaire
+// -------------------------------------------
+// -------------------------------------------
+
+function create_form_vue(section, tr_display_names, tr_names, tr_types, tr_values){
     // création du formulaire
 
-    let section = document.querySelector("#story");
     
     let form = document.createElement("form");
     section.appendChild(form);
@@ -29,29 +112,27 @@ function create_form_vue(tr_display_names, tr_names){
         // Infos
         let name = tr_names[i];
         let display_name = tr_display_names[i];
+        let type = tr_types[i];
+        let value = tr_values[i];
 
-        let tr = create_tr_line(display_name, name);
+        let tr = create_tr_line(display_name, name, type, value);
         table.appendChild(tr);
     }
-
-
-    let submit = create_input("Créer", "submit");
-    form.appendChild(submit);
 }
 
 
-function create_tr_line(display_name, name){
+function create_tr_line(display_name, name, type, value){
     // Ligne
     let tr = document.createElement("tr");
 
     // Case d'affichage de nom d'entrée
     let td_name = document.createElement("td");
-    td_name.innerHTML = display_name + " : "; // stoque le nom d'affichage dans la case
+    td_name.innerHTML = display_name; // stoque le nom d'affichage dans la case
     tr.appendChild(td_name);
 
     // Case d'entrée
     let td_input = document.createElement("td");
-    td_input.appendChild(create_input(name));
+    td_input.appendChild(create_input(name, type, value));
     tr.appendChild(td_input);
 
 
@@ -59,15 +140,13 @@ function create_tr_line(display_name, name){
 }
 
 
-function create_input(name, type="text"){
+function create_input(name, type, value){
     let input = document.createElement("input")
-    input.type = type;
 
-    if (type == "text"){
-        input.name = name;
-    } else {
-        input.value = name;
-    }
+    // Paramètre de l'entrée
+    input.type = type;
+    input.name = name;
+    input.value = value;
 
     return input;
 }
--- vue/admin_vue.php
+++ vue/admin_vue.php
@@ -2,16 +2,35 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>TEST Julie</title>
+    <title>Admin</title>
 
     <!-- Inports des scripts nécessaire au tests -->
     <script src="admin_fct_vue.js"></script>
 </head>
 
+<script>
+    function body_onload(){
+        // Story
+        create_story_form_vue(); 
+        update_story_form_vue(['', '', 'Modifier']);
+        delete_story_form_vue(['', 'Supprimer']);
 
-<body onload="create_story_form_vue();">
-    <h1>Page de <strong>TEST</strong> de Julie</h1>
+        // Character
+        create_char_form_vue();
+        update_char_form_vue(["", "", "", "", "Modifier"]);
+        delete_char_form_vue(["", "Supprimer"]);
+    }
+</script>
 
-    <section id="story"></section>
+<body onload="body_onload();">
+    <h1>Page Admin</h1>
+
+    <section id="story">
+        <h2>Histoire</h2>
+    </section>
+
+    <section id="character">
+        <h2>Personnage</h2>
+    </section>
 </body>
 </html>
